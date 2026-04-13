0599744aa1c170a626986b35c37d4e11c3b51d3a
Erick Navarro <navarroerick231@gmail.com>
2026-04-13 15:47:13 -0500

    no message

--- proyecto-vue/src/App.vue
+++ proyecto-vue/src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
+
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1> Hola mundo </h1>
+  <p>{{  contador }}</p>
+  <button @click="incrementar"> Incrementar </button>
+  <button @click="decrementar"> Decrementar </button>
 </template>
 
-<style scoped></style>
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+}
+const decrementar = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+color: red;
+
+}
+</style>
+
+
